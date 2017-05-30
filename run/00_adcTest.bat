d6935df4fa134090c0c73d3320e9a9ff48e87562
Stanislav Zhelnio <zhelniosl@yandex.ru>
2017-05-30 23:23:24 +0300

    testbench scripts prototypes added

---
+++ run/00_adcTest.bat
@@ -0,0 +1,7 @@
+rd /s /q sim
+md sim
+cd sim
+
+vsim -do ../00_adcTest.tcl
+
+cd ..
---
+++ run/00_adcTest.tcl
@@ -0,0 +1,47 @@
+
+# vlib work
+# 
+# set p0 -vlog01compat
+# set p1 +define+SIMULATION
+# 
+# set i0 +incdir+../../src/uart16550
+# set i1 +incdir+../../src/testbench
+# set i2 +incdir+../../src/
+# 
+# set s0 ../../src/uart16550/*.v
+# set s1 ../../src/testbench/*.v
+# set s2 ../../src/*.v
+# 
+# vlog $p0 $p1  $i0 $i1 $i2  $s0 $s1 $s2
+# 
+# vsim work.test_uart_transmit
+# add wave -radix hex sim:/test_uart_transmit/uart/*
+# run -all
+# wave zoom full
+
+vlib work
+
+vlog     ../../src/simulation/quartus_sim_lib/altera_primitives.v
+vlog     ../../src/simulation/quartus_sim_lib/220model.v
+vlog     ../../src/simulation/quartus_sim_lib/sgate.v
+vlog     ../../src/simulation/quartus_sim_lib/altera_mf.v
+vlog -sv ../../src/simulation/quartus_sim_lib/altera_lnsim.sv
+vlog     ../../src/simulation/quartus_sim_lib/fiftyfivenm_atoms.v
+vlog     ../../src/simulation/quartus_sim_lib/fiftyfivenm_atoms_ncrypt.v
+
+vlog     ../../src/simulation/qsys_submodules/altera_modular_adc_control.v
+vlog     ../../src/simulation/qsys_submodules/altera_modular_adc_control_avrg_fifo.v
+vlog     ../../src/simulation/qsys_submodules/altera_modular_adc_control_fsm.v
+vlog     ../../src/simulation/qsys_submodules/chsel_code_converter_sw_to_hw.v
+vlog     ../../src/simulation/qsys_submodules/fiftyfivenm_adcblock_primitive_wrapper.v
+vlog     ../../src/simulation/qsys_submodules/fiftyfivenm_adcblock_top_wrapper.v
+vlog     ../../src/simulation/qsys_submodules/adc_core_modular_adc_0.v
+vlog     ../../src/simulation/adc_core.v
+
+vlog     ../../src/*.v
+vlog     ../../src/simulation/testbench/*.v
+
+
+
+
+
